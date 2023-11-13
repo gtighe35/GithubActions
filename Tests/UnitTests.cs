d39083345ec3db53bcdad2c7bf0891f47993cd37
gtighe35 <gagetighe@gmail.com>
2023-11-13 13:55:04 -0600

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -34,9 +34,9 @@ namespace GithubActionsLab
          [Test]
         public void Subtract_Valid() //Should fail
         {
-            Assert.AreEqual(3, Program.Subtract("1", "2"));
-            Assert.AreEqual(5, Program.Subtract("3", "2"));
-            Assert.AreEqual(12, Program.Subtract("5", "7"));
+            Assert.AreEqual(-1, Program.Subtract("1", "2"));
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(-2, Program.Subtract("5", "7"));
         }
 
         [Test]
